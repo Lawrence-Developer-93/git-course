f7c337bd240eb677ba8d25d666d6835e01e3aa08
Virtue <DOLORES A. MARGAJA@DOLORES-MARGAJA.LAPTOP-TQESST6K>
2020-06-22 15:37:02 +0800

    trying to push again

---
+++ palindrome/index.js
@@ -0,0 +1,60 @@
+// --- Directions
+// Given a string, return true if the string is a palindrome
+// or false if it is not.  Palindromes are strings that
+// form the same word if it is reversed. *Do* include spaces
+// and punctuation in determining if the string is a palindrome.
+// --- Examples:
+//   palindrome("abba") === true
+//   palindrome("abcdefg") === false
+
+
+// function palindrome(str) {
+// 	return str.split('').every((char, i) => {
+// 		return char === str[str.length - i - 1];
+// 	}, 0)
+// }
+
+// module.exports = palindrome;
+
+
+
+
+
+// function palindrome(str) {
+
+// 	var reversed = str.split('').reverse().join('');
+
+// 	 return str === reversed;
+// }
+
+// module.exports = palindrome;
+
+
+
+// function palindrome(str) {
+// 	return str.split('').every((char, i) => {
+// 		return char === str[str.length - i - 1];
+// 	}, 0)
+// }
+
+// module.exports = palindrome;
+
+
+// function palindrome(str) {
+	
+// 	var x = str.split('').reverse().join('');
+// 	return x === str; 
+// }
+
+// module.exports = palindrome;
+
+
+
+
+// function palindrome(str) {
+// 	return str.split('').every((char, i) => {
+// 		return char === str[str.length - i - 1];
+// 	}, 0);
+// }
+
+// module.exports = palindrome;
